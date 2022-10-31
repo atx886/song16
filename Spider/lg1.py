70dcf00e6e457130109d92d20f3808845e3c863d
atx886 <56522871+atx886@users.noreply.github.com>
2022-10-31 09:46:47 +0800

    Update lg1.py

--- Spider/lg1.py
+++ Spider/lg1.py
@@ -1,6 +1,9 @@
+from telnetlib import EC
+
 from selenium import webdriver
 import time
 from openpyxl import load_workbook, Workbook
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 import os
 
@@ -12,36 +15,18 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 
-file = './Spider/16.xlsx'
-max_a = load_workbook(file).active.max_row
-print(max_a)
-
-
-def writeexcle(t):
-    wb = load_workbook(file)
-    sheet = wb.active
-    max_row = sheet.max_row - t
-
-    row_max = 'a' + str(max_row)
-    print(sheet)
-
-    if max_row > 0:
-        a = sheet[row_max].value
-        print(a)
-        return a
-    else:
-        print("空了")
-        return None
-
-
 # options = webdriver.FirefoxOptions()
 # options.set_headless(True)
 # options.add_argument("--headless")  # 设置火狐为headless无界面模式
 # options.add_argument("--disable-gpu")
 # d = webdriver.Firefox(options=options)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 # d = webdriver.Firefox()
-# # d = webdriver.Chrome()
+d = webdriver.Chrome()
+
+
 # d.implicitly_wait(5)
 
 
@@ -49,85 +34,58 @@ def rw():
     time.sleep(1)
 
 
-def dl(phone, d):
-    d.get('https://www.chaojijishi.com/h5/#/pages/login/login?from=user')
-    phh = d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[5]/uni-view[1]/uni-input/div/input')
+def dl(d):
+    # d.window_handles()
+    d.get('https://jiaobenwang.com/')
+    # WebDriverWait(d, time).until(EC.visibility_of_element_located((By.CLASS_NAME, 'visible_long_after_loading_only')))
+
+    d.find_element_by_xpath('/html/body/div[11]/div/div[1]/button').click()
+
+    # d.switch_to(d.window_handles[1])
+    # for handle in d.window_handles:  # 方法二，始终获得当前最后的窗口，所以多要多次使用
+    #     d.switch_to_window(handle)
+
+    # input()
+    rw()
+
+    d.find_element_by_xpath('//*[@id="navHeight"]/div/div/div[4]/div[1]').click()
 
     rw()
-    phh.send_keys(phone)
+    d.find_element_by_xpath('//*[@id="login"]/div/form/div[1]/input').send_keys('sch1532694569c@163.com')
 
     rw()
     d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[5]/uni-view[2]/uni-input/div/input').send_keys(
-        123456)
+        '//*[@id="login"]/div/form/div[2]/input').send_keys('123456789c')
     rw()
     d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[5]/uni-view[3]/uni-view[1]/uni-view').click()
-    rw()
+        '//*[@id="login"]/div/form/button').click()
+
+    time.sleep(5)
+
     d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[5]/uni-view[5]/uni-view/uni-view').click()
-    rw()
-    d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[5]/uni-view[4]').click()
-    rw()
+        '/html/body/div[3]/ul/li[1]/a/span').click()
+
+    time.sleep(3)
+    ax = d.find_element_by_xpath('//*[@id="swal2-title"]').text
+    print(ax)
+    return ax
+
+    # 鼠标悬停
+    # # 1、先定位到需要悬停的元素
+    # mte = d.find_element(By.CSS_SELECTOR, '.swal2-confirm')
+    #
+    # # 2、实现鼠标悬停 导入 from selenium.webdriver import ActionChains
+    # # ActionChains(浏览器).move_to_element(需要悬停的元素).perform()
+    # ActionChains(d).move_to_element(mte).perform()
+    #
+    # time.sleep(3)  # 悬停之后，最好等待1s中，悬停过程中，放下鼠标键盘
+    #
+    # # 3、点击悬停后 出现的元素
+    # d.find_element(By.LINK_TEXT, '.swal2-confirm').click()
 
 
-def zz(d, mm):
-    rw()
-    d.get('https://www.chaojijishi.com/h5/#/pages/subpack1/pay-model/JDpay')
-    rw()
-
-    jifen = d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view/uni-swiper/div/div/div/uni-swiper-item/uni-view/uni-view/uni-view[2]').text
-
-    print(jifen)
-    rw()
-    d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view/uni-view[1]/uni-view[1]').click()
-    rw()
-    # 输入号码
-    d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[2]/uni-view[2]/uni-view/uni-view/uni-view[2]/uni-view[2]/uni-input/div/input').send_keys(
-        16521637755)
-    rw()
-    # 输入积分
-    d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[2]/uni-view[2]/uni-view/uni-view/uni-view[3]/uni-view[2]/uni-input/div/input').send_keys(
-        jifen)
-
-    # 输入密码
-    rw()
-    d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[2]/uni-view[2]/uni-view/uni-view/uni-view[4]/uni-view[2]/uni-input/div/input').send_keys(
-        mm)
-    # 确定
-    rw()
-    # input('测试')
-    d.find_element_by_xpath(
-        '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[2]/uni-view[2]/uni-view/uni-view/uni-view[5]/uni-view[2]').click()
-    rw()
-    return jifen
-
-
-def cs():
-    a = max_a
-    c = 0
-    zong = 0
-    while a > 0:
-        d = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver)
-        # d = webdriver.Chrome()
-        # d = webdriver.Firefox()
-        d.implicitly_wait(5)
-        dl(writeexcle(c), d)
-        mm = "123456"
-        c += 1
-        zong += float(zz(d, mm))
-        a -= 1
-        d.close()
-
-    print("总共")
-    print(zong)
-
-
-cs()
+ax = dl(d)
+while ax != '今日已签到，请明日再来':
+    d.quit()
+    d = webdriver.Chrome
+    ax = dl(d)
